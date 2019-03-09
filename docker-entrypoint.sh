530a8dd6ed7532e50581383811c0856ed3df29bf
mloc <mloc@leela.netsoc.co>
2019-03-09 12:20:09 +0000

    import stuff

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
@@ -14,4 +14,20 @@ if [ ! -f /data/cms-contest-ready ]; then
     touch /data/cms-contest-ready
 fi
 
-cmsResourceService -a ALL
+if [ ! -f /data/cms-users-ready ]; then
+    if [[ -d "/cmsusers" ]]; then
+        cmsImportUser -L italy_yaml -c 1 -A /cmsusers
+    fi
+
+    if [[ -v CMS_ADMIN_PASS ]]; then
+        cmsAddAdmin admin -p "$CMS_ADMIN_PASS"
+    fi
+
+    if [[ -v CMS_TEST_USER ]]; then
+        cmsAddUser test test test -p test
+        cmsAddParticipation test -c 1
+    fi
+    touch /data/cms-users-ready
+fi
+
+cmsResourceService -a 1
